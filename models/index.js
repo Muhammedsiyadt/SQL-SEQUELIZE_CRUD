95866d3007091ffeb8f75ee506986fb0219a2c9b
Muhammedsiyadt <siyadkundala10@gmail.com>
2024-11-20 11:55:24 +0530

    updated

--- models/index.js
+++ models/index.js
@@ -5,14 +5,14 @@ const {Sequelize, DataTypes} = require('sequelize')
 const sequelize = new Sequelize(
     dbConfig.DB,
     dbConfig.USER,
-    dbConfig.PASSWORD, {
+    dbConfig.PASSWORD, { 
         host: dbConfig.HOST,
         dialect: dbConfig.dialect,
         operatorsAliases: false, 
-
+ 
         pool: {
             max: dbConfig.pool.max,
-            min: dbConfig.pool.min, 
+            min: dbConfig.pool.min,  
             acquire: dbConfig.pool.acquire,
             idle: dbConfig.pool.idle
         }
